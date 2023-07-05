a0d076fd440b2a03e00e0d0c62cec1e644f55740
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-07-05 12:39:46 -0600

    Added JScanner.java -Read .Json and .JPG.Json files

--- src/main/java/extractorMain/Main.java
+++ src/main/java/extractorMain/Main.java
@@ -1,7 +1,9 @@
 package extractorMain;
 
+import static handlers.JScanner.readJson;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        readJson();
     }
 }
---
+++ src/main/java/handlers/JScanner.java
@@ -0,0 +1,38 @@
+package handlers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class JScanner {
+    public static void readJson(){
+        ArrayList<File> jsonFiles = new ArrayList<>();
+        jsonFiles.add(Paths.get("src/sampleJson.json").toFile());
+        jsonFiles.add(Paths.get("src/samplePhotosJson.JPG.json").toFile());
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            jsonFiles.forEach((i) -> {
+               System.out.println("\n"+i);
+               Map<?,?> map = null;
+
+               try {
+                   map = objectMapper.readValue(i, Map.class);
+                   for (Map.Entry<?, ?> entry : map.entrySet()) {
+                       System.out.println(entry.getKey() + "=" + entry.getValue());
+                   }
+               } catch (IOException e) {
+                   throw new RuntimeException(e);
+               }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

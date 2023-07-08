5322ad51885e8997ec3c944cb2fd2074e45dc660
lumoyano <121978205+lumoyano@users.noreply.github.com>
2023-07-08 00:21:16 -0600

    Added IMGScanner.java

--- src/main/java/extractorMain/Main.java
+++ src/main/java/extractorMain/Main.java
@@ -1,9 +1,11 @@
 package extractorMain;
 
+import static handlers.IMGScanner.readIMG;
 import static handlers.JScanner.readJson;
 
 public class Main {
     public static void main(String[] args) {
         readJson();
+        readIMG();
     }
 }
